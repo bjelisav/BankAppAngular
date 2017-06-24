fc16ab2559dbd8bfb411fe7ef4abdf14ec900e53
bjelisav <boris.jelisavac@gmail.com>
2017-06-24 17:17:42 +0200

    Korigovan .path

--- js/controllers/addUserController.js
+++ js/controllers/addUserController.js
@@ -1,11 +1,12 @@
 angular.module('addUserController',['dbServices'])
-        .controller('addUserCtrl',function($scope,db,$location){
-          $location.path('/accounts');
+        .controller('addUserCtrl',function($scope,db,$location,$log){
+
           $scope.sendData = function(){
             db.sendData({
               name : $scope.name,
               deposit : $scope.deposit,
               card : $scope.card
             })
+            $location.path("/accounts");
           };
         })
